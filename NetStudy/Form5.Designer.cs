3b15f94e53cf68f30a15cdaaaf04ceffe55d02b9
ngokimthanh1455@gmail.com <your-email@example.com>
2024-10-09 12:30:35 +0700

    Thêm Form thông tin tài khoản

---
+++ NetStudy/Form5.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace NetStudy
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form5));
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(25, 164);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(198, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(258, 164);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(198, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(258, 255);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(198, 27);
+            textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(25, 255);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(198, 27);
+            textBox4.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(258, 339);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(198, 27);
+            textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(25, 339);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(198, 27);
+            textBox6.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.Font = new Font("#9Slide03 Cabin Bold", 10.1999989F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(67, 417);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 40);
+            button1.TabIndex = 6;
+            button1.Text = "CHỈNH SỬA";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.BackgroundImage = (Image)resources.GetObject("button2.BackgroundImage");
+            button2.Font = new Font("#9Slide03 Cabin Bold", 10.1999989F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(282, 417);
+            button2.Name = "button2";
+            button2.Size = new Size(127, 40);
+            button2.TabIndex = 7;
+            button2.Text = "XÁC NHẬN";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form5
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(468, 560);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox6);
+            Controls.Add(textBox3);
+            Controls.Add(textBox4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form5";
+            Text = "Form5";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Button button1;
+        private Button button2;
+    }
+}
