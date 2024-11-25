67f496e77099b7a62d9505851eb1cdfe17033fa2
ngokimthanh1455@gmail.com <your-email@example.com>
2024-11-25 11:18:40 +0700

    mainmenu1

--- NetStudy/MainMenu.Designer.cs
+++ NetStudy/MainMenu.Designer.cs
@@ -29,19 +29,189 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+            panelMenu = new Panel();
+            panelLogo = new Panel();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            iconButton2 = new FontAwesome.Sharp.IconButton();
+            iconButton3 = new FontAwesome.Sharp.IconButton();
+            iconButton4 = new FontAwesome.Sharp.IconButton();
+            iconButton5 = new FontAwesome.Sharp.IconButton();
+            iconButton6 = new FontAwesome.Sharp.IconButton();
+            panelMenu.SuspendLayout();
             SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            panelMenu.BackColor = Color.FromArgb(31, 30, 68);
+            panelMenu.Controls.Add(iconButton6);
+            panelMenu.Controls.Add(iconButton5);
+            panelMenu.Controls.Add(iconButton4);
+            panelMenu.Controls.Add(iconButton3);
+            panelMenu.Controls.Add(iconButton2);
+            panelMenu.Controls.Add(iconButton1);
+            panelMenu.Controls.Add(panelLogo);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(220, 631);
+            panelMenu.TabIndex = 0;
+            // 
+            // panelLogo
+            // 
+            panelLogo.Dock = DockStyle.Top;
+            panelLogo.Location = new Point(0, 0);
+            panelLogo.Name = "panelLogo";
+            panelLogo.Size = new Size(220, 140);
+            panelLogo.TabIndex = 0;
+            // 
+            // iconButton1
+            // 
+            iconButton1.Dock = DockStyle.Top;
+            iconButton1.FlatAppearance.BorderSize = 0;
+            iconButton1.FlatStyle = FlatStyle.Flat;
+            iconButton1.ForeColor = Color.Gainsboro;
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.Star;
+            iconButton1.IconColor = Color.Gainsboro;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.IconSize = 32;
+            iconButton1.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton1.Location = new Point(0, 140);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Padding = new Padding(10, 0, 20, 0);
+            iconButton1.Size = new Size(220, 60);
+            iconButton1.TabIndex = 1;
+            iconButton1.Text = "Tổng quan";
+            iconButton1.TextAlign = ContentAlignment.MiddleLeft;
+            iconButton1.TextImageRelation = TextImageRelation.ImageBeforeText;
+            iconButton1.UseVisualStyleBackColor = true;
+            // 
+            // iconButton2
+            // 
+            iconButton2.Dock = DockStyle.Top;
+            iconButton2.FlatAppearance.BorderSize = 0;
+            iconButton2.FlatStyle = FlatStyle.Flat;
+            iconButton2.ForeColor = Color.Gainsboro;
+            iconButton2.IconChar = FontAwesome.Sharp.IconChar.File;
+            iconButton2.IconColor = Color.Gainsboro;
+            iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton2.IconSize = 32;
+            iconButton2.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton2.Location = new Point(0, 200);
+            iconButton2.Name = "iconButton2";
+            iconButton2.Padding = new Padding(10, 0, 20, 0);
+            iconButton2.Size = new Size(220, 60);
+            iconButton2.TabIndex = 2;
+            iconButton2.Text = "Tài liệu";
+            iconButton2.TextAlign = ContentAlignment.MiddleLeft;
+            iconButton2.TextImageRelation = TextImageRelation.ImageBeforeText;
+            iconButton2.UseVisualStyleBackColor = true;
+            // 
+            // iconButton3
+            // 
+            iconButton3.Dock = DockStyle.Top;
+            iconButton3.FlatAppearance.BorderSize = 0;
+            iconButton3.FlatStyle = FlatStyle.Flat;
+            iconButton3.ForeColor = Color.Gainsboro;
+            iconButton3.IconChar = FontAwesome.Sharp.IconChar.Comment;
+            iconButton3.IconColor = Color.Gainsboro;
+            iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton3.IconSize = 32;
+            iconButton3.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton3.Location = new Point(0, 260);
+            iconButton3.Name = "iconButton3";
+            iconButton3.Padding = new Padding(10, 0, 20, 0);
+            iconButton3.Size = new Size(220, 60);
+            iconButton3.TabIndex = 3;
+            iconButton3.Text = "Đoạn chat";
+            iconButton3.TextAlign = ContentAlignment.MiddleLeft;
+            iconButton3.TextImageRelation = TextImageRelation.ImageBeforeText;
+            iconButton3.UseVisualStyleBackColor = true;
+            // 
+            // iconButton4
+            // 
+            iconButton4.Dock = DockStyle.Top;
+            iconButton4.FlatAppearance.BorderSize = 0;
+            iconButton4.FlatStyle = FlatStyle.Flat;
+            iconButton4.ForeColor = Color.Gainsboro;
+            iconButton4.IconChar = FontAwesome.Sharp.IconChar.UsersRectangle;
+            iconButton4.IconColor = Color.Gainsboro;
+            iconButton4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton4.IconSize = 32;
+            iconButton4.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton4.Location = new Point(0, 320);
+            iconButton4.Name = "iconButton4";
+            iconButton4.Padding = new Padding(10, 0, 20, 0);
+            iconButton4.Size = new Size(220, 60);
+            iconButton4.TabIndex = 4;
+            iconButton4.Text = "Lớp học";
+            iconButton4.TextAlign = ContentAlignment.MiddleLeft;
+            iconButton4.TextImageRelation = TextImageRelation.ImageBeforeText;
+            iconButton4.UseVisualStyleBackColor = true;
+            // 
+            // iconButton5
+            // 
+            iconButton5.Dock = DockStyle.Top;
+            iconButton5.FlatAppearance.BorderSize = 0;
+            iconButton5.FlatStyle = FlatStyle.Flat;
+            iconButton5.ForeColor = Color.Gainsboro;
+            iconButton5.IconChar = FontAwesome.Sharp.IconChar.PenRuler;
+            iconButton5.IconColor = Color.Gainsboro;
+            iconButton5.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton5.IconSize = 32;
+            iconButton5.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton5.Location = new Point(0, 380);
+            iconButton5.Name = "iconButton5";
+            iconButton5.Padding = new Padding(10, 0, 20, 0);
+            iconButton5.Size = new Size(220, 60);
+            iconButton5.TabIndex = 5;
+            iconButton5.Text = "Ôn tập";
+            iconButton5.TextAlign = ContentAlignment.MiddleLeft;
+            iconButton5.TextImageRelation = TextImageRelation.ImageBeforeText;
+            iconButton5.UseVisualStyleBackColor = true;
+            // 
+            // iconButton6
+            // 
+            iconButton6.Dock = DockStyle.Top;
+            iconButton6.FlatAppearance.BorderSize = 0;
+            iconButton6.FlatStyle = FlatStyle.Flat;
+            iconButton6.ForeColor = Color.Gainsboro;
+            iconButton6.IconChar = FontAwesome.Sharp.IconChar.Handshake;
+            iconButton6.IconColor = Color.Gainsboro;
+            iconButton6.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton6.IconSize = 32;
+            iconButton6.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton6.Location = new Point(0, 440);
+            iconButton6.Name = "iconButton6";
+            iconButton6.Padding = new Padding(10, 0, 20, 0);
+            iconButton6.Size = new Size(220, 60);
+            iconButton6.TabIndex = 6;
+            iconButton6.Text = "Bạn học";
+            iconButton6.TextAlign = ContentAlignment.MiddleLeft;
+            iconButton6.TextImageRelation = TextImageRelation.ImageBeforeText;
+            iconButton6.UseVisualStyleBackColor = true;
             // 
             // MainMenu
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1121, 631);
+            Controls.Add(panelMenu);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "MainMenu";
             Text = "NETSTUDY";
+            panelMenu.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panelMenu;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private Panel panelLogo;
+        private FontAwesome.Sharp.IconButton iconButton6;
+        private FontAwesome.Sharp.IconButton iconButton5;
+        private FontAwesome.Sharp.IconButton iconButton4;
+        private FontAwesome.Sharp.IconButton iconButton3;
+        private FontAwesome.Sharp.IconButton iconButton2;
     }
 }
